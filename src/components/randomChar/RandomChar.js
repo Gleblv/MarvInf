114a9d785a7828cec39c06f921ed984dc81c7c86
Gleb <lv.gleb44@gmail.com>
2022-04-08 17:09:04 +0300

    add spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,41 +1,86 @@
+import { Component } from 'react/cjs/react.production.min';
+
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner';
+
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar = () => {
-    return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+class RandomChar extends Component {
+    constructor (props) {
+        super(props);
+        this.updateChar(); // вызываем метод один раз чтобы при загрузке сайта отрендерился случайный персонаж
+    }
+
+    state = {
+        character: {
+        
+        },
+        loading: true
+    }
+
+    marvelService = new MarvelService(); // создаём новое свойство. Тоже самое что this.marvelService = ... ;
+
+    onCharacterLoaded = (character) => { // перекидываем пришедшие данные в state
+        this.setState({
+            character, 
+            loading: false
+        });
+    }
+
+    updateChar = () => {
+        const id = ~~(Math.random() * (1011400 - 1011000) + 1011000); // случайное число в промежутке от 1011000 до 1011400
+        this.marvelService
+            .getCharacteer(id) // получаем данные об одном персонаже (Метод написан в папке servesec/MarvelServise.js)
+            .then(this.onCharacterLoaded) // пришедший объект сразу видоизменяется после метода getCharacteer. Пришедший аргумент автоматически подставляется в метод
+    }
+
+    render () {
+        const {character, loading} = this.state;
+
+        return (
+            <div className="randomchar">
+                {/* Подставляем либо компонент с вёрсткой (смотреть ниже) либо компонент с svg спинером */}
+                {loading ? <Spinner/> : <View character={character}/>} 
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
                     </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
                 </div>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-        </div>
+        )
+    }
+}
+
+const View = ({character}) => { // вынесли часть вёстки из гланого компонента
+    const {name, description, thumbnail, homepage, wiki} = character;
+
+    return (
+        <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                            {description}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
     )
 }
 
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,12 +14,33 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacteers = () => { // Метод чтобы получить всех персонажей
-        return this.getResoursec(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacteers = async () => { // Метод чтобы получить всех персонажей
+        const res = await this.getResoursec(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`); // записываем ответ от сервера в переменную
+        return res.data.results.map(this._transformCharacter); // передаём функцию по которой будем изменять элементы массива
     }
 
-    getCharacteer = (id) => { // Метод чтобы получить одного персонажа по id
-        return this.getResoursec(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacteer = async (id) => { // Метод чтобы получить одного персонажа по id
+        const res = await this.getResoursec(`${this._apiBase}characters/${id}?${this._apiKey}`); // записываем ответ от сервера в переменную
+
+        if (res.data.results[0].description === "") { // если нет описания то вставляем заглушку
+            res.data.results[0].description = "Description of the character is temporarily unavailable";
+        } 
+
+        if (res.data.results[0].description.length > 220) { // если описание слишеом длинное то обрезаем
+            res.data.results[0].description.substring = res.data.results[0].description.substring(0, 220);
+        }
+
+        return this._transformCharacter(res.data.results[0]); // возвращаем уже видоизменённый объект
+    }
+
+    _transformCharacter = (character) => { // передаём индекс нужного персонажа из массива пришедшего с API
+        return {
+            name: character.name,
+            description: character.description,
+            thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
+            homepage: character.urls[0].url,
+            wiki: character.urls[1].url
+        }
     }
 }
 
