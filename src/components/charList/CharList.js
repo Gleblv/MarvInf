62bd193905ad52d1dca167fa60aaf8c9d7b98a9a
Gleb <lv.gleb44@gmail.com>
2022-04-17 15:24:02 +0300

    loading characters

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,56 @@
+import { Component } from 'react/cjs/react.production.min';
+
+import MarvelService from '../../services/MarvelService';
+
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
+class CharList extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            characters: []
+        }
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount () {
+        this.marvelService
+            .getAllCharacteers()
+            .then(data => this.setState(({characters: data}))) // помещаем ответ от сервера  с массивом персонажей в state
+                        
+    }
+
+    pushCharacters = () => {
+        const charactersList = this.state.characters.map(item => { // перебираем массив из state и возвращаем массив с элементами списка
+            return (
+                <li className='char__item' key={item.id}>
+                    <img src={item.thumbnail} alt={item.name}/>
+                    <div className="char__name">{item.name}</div>
                 </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+            )
+        })
+
+        return ( // возвращаем список с нашим массивом
+            <ul className='char__grid'>
+                {charactersList}
             </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+        )
+    }
+
+    render () { 
+        const content = this.pushCharacters();
+
+        return (
+            <div className="char__list">
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
 }
 
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,11 +8,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor (props) {
-        super(props);
-        this.updateChar(); // вызываем метод один раз чтобы при загрузке сайта отрендерился случайный персонаж
-    }
-
     state = {
         character: {
         
@@ -22,6 +17,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService(); // создаём новое свойство. Тоже самое что this.marvelService = ... ;
+
+    componentDidMount() {
+        this.updateChar(); // вызываем метод один раз чтобы при загрузке сайта отрендерился случайный персонаж
+    }
 
     onCharacterLoaded = (character) => { // перекидываем пришедшие данные в state
         this.setState({
@@ -65,7 +64,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -78,9 +77,23 @@ class RandomChar extends Component {
 const View = ({character}) => { // вынесли часть вёстки из гланого компонента
     const {name, description, thumbnail, homepage, wiki} = character;
 
+    let thumbnailStyle = {
+        width: '180px',
+        height: '180px',
+        objectFit: 'cover'
+    }
+
+    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") { // если сообщение с ошибкой, то немного меняем стили изображения
+        thumbnailStyle = {
+            width: '180px',
+            height: '180px',
+            objectFit: 'contain'
+        }
+    }
+
     return (
         <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <img src={thumbnail} style={thumbnailStyle} alt="Random character" className="randomchar__img"/>
                     <div className="randomchar__info">
                         <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
